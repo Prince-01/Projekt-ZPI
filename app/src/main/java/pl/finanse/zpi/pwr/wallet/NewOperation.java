af0793419772391b59dbffcb19ecc9a0119bdd0e
Robert <robert21z@o2.pl>
2016-04-08 13:07:07 +0200

    keyboard mode made

--- app/src/main/java/pl/finanse/zpi/pwr/wallet/NewOperation.java
+++ app/src/main/java/pl/finanse/zpi/pwr/wallet/NewOperation.java
@@ -1,12 +1,16 @@
 package pl.finanse.zpi.pwr.wallet;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -27,6 +31,31 @@ public class NewOperation extends Fragment {
         Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Montserrat-Regular.otf");
         text.setTypeface(tf);
         // Inflate the layout for this fragment
+        setupUI(view);
         return view;
+    }
+
+    public void setupUI(View view) {
+        //Set up touch listener for non-text box views to hide keyboard.
+        if(!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(getActivity());
+                    return false;
+                }
+            });
+        }
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
 }
