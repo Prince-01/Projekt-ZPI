48255701aab16c6668d6269353287f36745e4cee
Woxis17 <robert21z@o2.pl>
2016-05-13 14:18:45 +0200

    history filter works fine

--- app/src/main/java/pl/finanse/zpi/pwr/wallet/views/HistoryView.java
+++ app/src/main/java/pl/finanse/zpi/pwr/wallet/views/HistoryView.java
@@ -97,7 +97,6 @@ Wypełniamy listę dla wybranego przedzialu daty operacje
 
     public static class DatePickerFragment extends DialogFragment
             implements DatePickerDialog.OnDateSetListener {
-
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             Button from = (Button)getActivity().findViewById(R.id.dateFromButton);
@@ -108,12 +107,12 @@ Wypełniamy listę dla wybranego przedzialu daty operacje
             } else {
                 date = to.getText().toString();
             }
-            final Calendar c = Calendar.getInstance(); //04.04.2016
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+            final Calendar c = Calendar.getInstance();
+            int year = Integer.parseInt(date.substring(6));
+            int month = Integer.parseInt(date.substring(3,5));// c.get(Calendar.MONTH);
+            int day = Integer.parseInt(date.substring(0,2));
             // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+            return new DatePickerDialog(getActivity(), this, year, month-1, day);
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
